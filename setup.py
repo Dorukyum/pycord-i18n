3701a3eb711f34d4e8b624064ca83026cd78b019
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2022-09-03 12:53:17 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+classifiers = [
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="",
+    packages=[""],
+    version="",
+    license="MIT",
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="",
+    url="",
+    keywords="",
+    install_requires=[""],
+    classifiers=classifiers,
+    project_urls={"Source": ""},
+)
