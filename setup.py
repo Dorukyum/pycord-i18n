50744a9998d02cf258bf83c4de7e757f79628aeb
Dorukyum <doruk.ak@hotmail.com>
2022-09-04 16:02:11 +0300

    Update project metadata

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-    name="",
-    packages=[""],
-    version="",
+    name="pycord-i18n",
+    packages=["pycord.i18n"],
+    version="1.0.0",
     license="MIT",
-    description="",
+    description="Internationalization for pycord",
     long_description=readme,
     long_description_content_type="text/markdown",
-    author="",
-    url="",
-    keywords="",
-    install_requires=[""],
+    author="Dorukyum",
+    url="https://github.com/Dorukyum/pycord-i18n",
+    keywords="Pycord",
+    install_requires=["py-cord>=2.0.0"],
     classifiers=classifiers,
-    project_urls={"Source": ""},
+    project_urls={"Source": "https://github.com/Dorukyum/pycord-i18n"},
 )
